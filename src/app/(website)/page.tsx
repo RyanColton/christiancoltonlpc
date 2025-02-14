668f14a5a2b6d893e62bb0a89b0621da3345cef0
Ryan Colton <ryan.m.colton@gmail.com>
2025-02-14 14:01:26 -0600

    moving svg section to its own section

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,12 +1,31 @@
 import { Hero } from "@/components/Layout/Hero";
 import Services from "./services/Services";
 import Contact from "./contact/Contact";
+import Insurance from "@/svgs/Insurance";
+import UserCheck from "@/svgs/UserCheck";
+import CreditCard from "@/svgs/CreditCard";
 
 export default function Home() {
   return (
     <div className="h-full flex flex-col justify-center items-center" >
       <Hero />
       <Services shouldFadeOnScroll />
+      <section className="relative bg-neutral-50 px-4 py-8 sm:p-20 w-full flex flex-col items-center justify-center">
+        <div className="max-w-12 sm:max-w-7xl mt-6 sm:mt-8 mx-auto flex flex-col sm:flex-row gap-12 items-center justify-centerp-4">
+          <div className="flex flex-col w-40 gap-2 items-center justify-center">
+            <Insurance className="h-24 sm:h-36" />
+            <p className="text-xl text-center"> Accepts Insurance</p>
+          </div>
+          <div className="flex flex-col w-40 gap-2 items-center justify-center">
+            <CreditCard className="h-24 sm:h-36" />
+            <p className="text-xl text-center">Accepts Online Payments</p>
+          </div>
+          <div className="flex flex-col w-40 gap-2 items-center justify-center">
+            <UserCheck className="h-24 sm:h-36" />
+              <p className="text-xl text-center">Waitlisting New Clients</p>
+            </div>
+          </div>
+      </section>
       <Contact shouldFadeOnScroll />
     </div>
   );
--- src/components/Layout/Hero.tsx
+++ src/components/Layout/Hero.tsx
@@ -11,11 +11,6 @@ export function Hero() {
     <section className="relative bg-neutral-50 px-4 py-8 sm:p-20 w-full flex flex-col items-center justify-center">
       <div className="max-w-7xl mx-auto">
         <div className="text-center">
-          <FadeIn>
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-neutral-900">
-              Christian Colton LPC
-            </h1>
-          </FadeIn>
           <FadeIn delay={200}>
             <div className="flex flex-col sm:flex-row items-center justify-between gap-4 mt-4">
               <img
@@ -34,22 +29,6 @@ export function Hero() {
         <div className="mt-6 sm:mt-8">
           <Button variant="primary" href="/contact">Get Started</Button>
         </div>
-      </FadeIn>
-      <FadeIn delay={800}>
-        <div className="max-w-12 sm:max-w-7xl mt-6 sm:mt-8 mx-auto flex flex-col sm:flex-row gap-12 items-center justify-center">
-          <div className="flex flex-col w-40 gap-2 items-center justify-center">
-            <Insurance className="h-24 sm:h-36" />
-            <p className="text-xl text-center"> Accepts Insurance</p>
-          </div>
-          <div className="flex flex-col w-40 gap-2 items-center justify-center">
-            <CreditCard className="h-24 sm:h-36" />
-            <p className="text-xl text-center">Accepts Online Payments</p>
-          </div>
-          <div className="flex flex-col w-40 gap-2 items-center justify-center">
-            <UserCheck className="h-24 sm:h-36" />
-              <p className="text-xl text-center">Waitlisting New Clients</p>
-            </div>
-          </div>
       </FadeIn>
     </section>
   )
